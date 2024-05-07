8550b21785f658fc3989b7425d25146557cb184a
divyansh rastogi <rastogi11204@gmail.com>
2024-05-07 22:24:46 +0530

    routing improved 1.1

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import "./Sidebar.css"
 const Sidebar = () => {
   return (
       <div className="App-Sidebar">  
-        <div id="option1"><Link to="/contact">Contact Page</Link></div>
+        <div id="option1"><Link to="/">Contact Page</Link></div>
         <div id="option2"><Link to="/map">Map and Chart</Link></div>
       </div> 
   )
